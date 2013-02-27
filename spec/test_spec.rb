6cadb70f144b1ae13bf6a3b34a01c11da0a7f2bb
James Cuzella <james.cuzella@lyraphase.com>
2013-02-27 13:04:40 -0700

    Use OS.stub in multiple contexts

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,25 +1,53 @@
 require 'spec_helper'
 require 'testme'
 
-describe "TestMe" do
-  it "should work on linux" do
+## This works
+#describe "TestMe" do
+#  it "should work on linux" do
+#      OS.stub(linux?: true, mac?: false, doze?: false, bits: 64)
+#
+#      test_this = TestMe.new()
+#      test_this.get_os.should eq 'linux64'
+#  end
+#
+#  it "should work on windows" do
+#      OS.stub(linux?: false, mac?: false, doze?: true, bits: 64)
+#
+#      test_this = TestMe.new()
+#      test_this.get_os.should eq 'win'
+#  end
+#
+#  it "should work on mac" do
+#      OS.stub(linux?: false, mac?: true, doze?: false, bits: 64)
+#
+#      test_this = TestMe.new()
+#      test_this.get_os.should eq 'mac'
+#  end
+#end
+#
+
+# And this works too
+describe "#get_os" do
+
+  subject { TestMe.new.get_os }
+
+  context "on a linux machine" do
+
+    before do
       OS.stub(linux?: true, mac?: false, doze?: false, bits: 64)
+    end
 
-      test_this = TestMe.new()
-      test_this.get_os.should eq 'linux64'
+    it { should eq 'linux64' }
   end
 
-  it "should work on windows" do
-      OS.stub(linux?: false, mac?: false, doze?: true, bits: 64)
+  context "on a mac" do
 
-      test_this = TestMe.new()
-      test_this.get_os.should eq 'win'
-  end
-
-  it "should work on mac" do
+    before do
       OS.stub(linux?: false, mac?: true, doze?: false, bits: 64)
+    end
 
-      test_this = TestMe.new()
-      test_this.get_os.should eq 'mac'
+    it { should eq 'mac' }
   end
+
+  # etc etc
 end
