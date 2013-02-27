a4b8f3395878276dd0746a9b4ffa53a7e58324e9
James Cuzella <james.cuzella@lyraphase.com>
2013-02-27 12:58:27 -0700

    Use OS.stub to stub out the methods

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,45 +3,21 @@ require 'testme'
 
 describe "TestMe" do
   it "should work on linux" do
-      OS.class_eval do
-        def linux?
-          return true
-        end
-        def bits
-          return 32
-        end
-      end
-      
+      OS.stub(linux?: true, mac?: false, doze?: false, bits: 64)
+
       test_this = TestMe.new()
-      test_this.get_os.should eq 'linux32'
+      test_this.get_os.should eq 'linux64'
   end
 
   it "should work on windows" do
-      OS.class_eval do
-          def linux?
-              return false
-          end
-          def mac?
-              return false
-          end
-          def doze?
-              return true
-          end
-      end
+      OS.stub(linux?: false, mac?: false, doze?: true, bits: 64)
 
       test_this = TestMe.new()
       test_this.get_os.should eq 'win'
   end
 
   it "should work on mac" do
-      OS.class_eval do
-          def linux?
-              return false
-          end
-          def mac?
-              return true
-          end
-      end
+      OS.stub(linux?: false, mac?: true, doze?: false, bits: 64)
 
       test_this = TestMe.new()
       test_this.get_os.should eq 'mac'
